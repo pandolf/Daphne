1647cf1ed86c0b3ff43eefb0df6924e31c1f458d
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-05-24 12:23:01 +0200

    reports every 100

--- GenParticleAnalyzer/python/ConfFile_cfg.py
+++ GenParticleAnalyzer/python/ConfFile_cfg.py
@@ -4,19 +4,9 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-#process.MessageLogger = cms.Service("MessageLogger",
-#                                              destinations   = cms.untracked.vstring('detailedInfo'),
-#                                              categories      = cms.untracked.vstring('eventNumber'),
-#                                              detailedInfo    = cms.untracked.PSet(
-#                                                                        eventNumber = cms.untracked.PSet(
-#                                                                                                  reportEvery = cms.untracked.int32(100)
-#                                                                                                )
-#                                                                                                                                 ),
-#)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     ## replace 'myfile.root' with the source file you want to use
@@ -27,11 +17,10 @@ process.source = cms.Source("PoolSource",
     #fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/PhaseISpring17DR/SingleNeutrino/GEN-SIM-RECO/FlatPU0to75RECO_90X_upgrade2017_realistic_v20-v1/120000/001DD274-7240-E711-BB47-A0369FC5EEF4.root')
     #fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/QCD_HT100to200_TuneCP5_13TeV-madgraph-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/50000/0024A35F-86EB-E711-A5BC-A4BF0112BC06.root')
     #fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/QCD_HT100to200_TuneCP5_13TeV-madgraph-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/50000/0024A35F-86EB-E711-A5BC-A4BF0112BC06.root')
-    fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/RunIIWinter17DR/QCD_Pt-15to3000_TuneCP5_Flat_13TeV_pythia8/GEN-SIM-RAW/NZSPU0to70_94X_upgrade2018_realistic_v8-v1/00000/0003F19E-74FF-E711-B73B-FA163E697F46.root')
 
+    #fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/RunIIWinter17DR/QCD_Pt-15to3000_TuneCP5_Flat_13TeV_pythia8/GEN-SIM-RAW/NZSPU0to70_94X_upgrade2018_realistic_v8-v1/00000/0003F19E-74FF-E711-B73B-FA163E697F46.root')
 
-
-
+    fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/RunIIFall17GS/QCD_Pt_15to30_TuneCP5_13TeV_pythia8/GEN-SIM/93X_mc2017_realistic_v3-v1/150000/008099F0-81B2-E711-8F1A-0CC47A4C8EE8.root')
 
 
 
