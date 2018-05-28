3b9274a0183a522fee917daf9f2099241929b0d9
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-05-28 16:17:44 +0200

    added commented line

--- GenParticleAnalyzer/python/ConfFile_cfg.py
+++ GenParticleAnalyzer/python/ConfFile_cfg.py
@@ -5,6 +5,7 @@ process = cms.Process("Demo")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 
