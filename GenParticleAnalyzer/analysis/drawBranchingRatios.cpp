fcf2c08143f9a389c0b5fcc0339ffa8c48c5de2e
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-06-05 12:24:41 +0200

    now with 21% br in K decay

--- GenParticleAnalyzer/analysis/drawBranchingRatios.cpp
+++ GenParticleAnalyzer/analysis/drawBranchingRatios.cpp
@@ -17,9 +17,9 @@ int main() {
 
   TH2D* h2_axes = new TH2D( "axes", "", 10, 0., xMax, 10, 1.5E-7, 1. );
   h2_axes->SetXTitle( "m_{A'} [MeV]" );
-  h2_axes->SetYTitle( "BR( X ) / #epsilon^{2}" );
+  h2_axes->SetYTitle( "BR( X ) / #epsilon ^{2}" );
 
-  TF1* br_pizero = new TF1("br_pizero", "TMath::Max( 0., TMath::Power( 1.-x*x/(135.*135.) , 3. ))", 0., xMax);
+  TF1* br_pizero = new TF1("br_pizero", "TMath::Max( 0., 0.21*TMath::Power( 1.-x*x/(135.*135.) , 3. ))", 0., xMax);
   br_pizero->SetLineColor(46); 
   br_pizero->SetLineWidth(2); 
 
@@ -36,10 +36,10 @@ int main() {
   br_pizero->Draw("L same" );
   br_kappa3->Draw("L same" );
 
-  TLegend* legend = new TLegend( 0.55, 0.75, 0.9, 0.9 );
+  TLegend* legend = new TLegend( 0.5, 0.75, 0.9, 0.9 );
   legend->SetFillColor(0);
   legend->SetTextSize(0.035);
-  legend->AddEntry( br_pizero, "#pi^{0} #rightarrow A' #gamma", "L" );
+  legend->AddEntry( br_pizero, "K^{#pm} #rightarrow #pi^{#pm}#pi^{0} #rightarrow #pi^{#pm}A'#gamma", "L" );
   legend->AddEntry( br_kappa3, "K^{#pm} #rightarrow #pi^{#pm} A'", "L" );
   legend->Draw("same");
 
@@ -59,10 +59,10 @@ int main() {
   br_kappa3->Draw("L same" );
   br_delta->Draw("L same");
 
-  TLegend* legend2 = new TLegend( 0.55, 0.72, 0.9, 0.9 );
+  TLegend* legend2 = new TLegend( 0.5, 0.72, 0.9, 0.9 );
   legend2->SetFillColor(0);
   legend2->SetTextSize(0.035);
-  legend2->AddEntry( br_pizero, "#pi^{0} #rightarrow A' #gamma", "L" );
+  legend2->AddEntry( br_pizero, "K^{#pm} #rightarrow #pi^{#pm}#pi^{0} #rightarrow #pi^{#pm}A'#gamma", "L" );
   legend2->AddEntry( br_kappa3, "K^{#pm} #rightarrow #pi^{#pm} A'", "L" );
   legend2->AddEntry( br_delta , "#Delta #rightarrow p A'", "L" );
   legend2->Draw("same");
