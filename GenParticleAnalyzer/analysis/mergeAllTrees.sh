497103f5308ac2cb091511949f3dc9e6a5bf725c
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-07-13 17:53:43 +0200

    updated to genTreeMerger

--- GenParticleAnalyzer/analysis/mergeAllTrees.sh
+++ GenParticleAnalyzer/analysis/mergeAllTrees.sh
@@ -2,5 +2,5 @@ if [ $# -lt 1 ]
 then
   echo "Please provide production name and pass it as first argument: ./mergeAllTrees.sh [productionName]"
 else
-  ./mergeTrees $1 QCD_Pt_15to30
+  ./genTreeMerger $1 QCD_Pt_15to30
 fi
